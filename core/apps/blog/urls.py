76d7459699e32d3fe085aca58babc7f3e205724e
Mohamad Ep <mamad_ep@yahoo.com>
2025-10-23 19:59:22 +0330

    Feat: Add Configure blog by CBV in blog app
    Adding blog classes and performing CRUD operations in render base

--- core/apps/blog/urls.py
+++ core/apps/blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path,include
+from django.urls import path
 from apps.blog import views as views
 # _______________________________________________________
 
@@ -6,6 +6,12 @@ app_name = 'blog'
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name='index'),
+    path("post-list/", views.ArticleListView.as_view(), name='post-list'),
+    path("create-post/", views.CreateArticleView.as_view(), name='create-post'),
+    path("update-post/<str:slug>/", views.UpdatePostView.as_view(), name='update-post'),
+    path("delete-post/<str:slug>/", views.DeletePostView.as_view(), name='delete-post'),
+    path("blog/", views.BlogView.as_view(), name='posts'),
+    path("blog/<str:slug>/", views.PostDetailsView.as_view(), name='post-details'),
 
 ]
 
--- core/apps/blog/views.py
+++ core/apps/blog/views.py
@@ -1,7 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView,FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Article
+from apps.accounts.models import Profile
+from .forms import ArticleForm
+from django.urls import reverse_lazy
+from django.conf import settings
+from django.views import View
+# _______________________________________________________
+
+def media_admin(request):
+    return {"media_url":settings.MEDIA_URL}
 # _______________________________________________________
 
 class IndexView(TemplateView):
     template_name = 'blog/index.html'
+# _______________________________________________________
+
+class ArticleListView(LoginRequiredMixin,ListView):
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = '-published_date'
+    
+    def get_queryset(self):
+        profile = get_object_or_404(Profile,user=self.request.user)
+        return Article.objects.filter(is_active=True,author=profile)
+    
+# _______________________________________________________
+
+class CreateArticleView(LoginRequiredMixin,CreateView):
+    template_name = 'blog/create_post.html'
+    form_class = ArticleForm
+    success_url = reverse_lazy('blog:post-list')
+    model = Article
+    
+    def form_valid(self, form):
+        profile = get_object_or_404(Profile,user=self.request.user)
+        form.instance.author = profile
+        form.save()
+        return super().form_valid(form)
+    
+# _______________________________________________________
+
+class UpdatePostView(LoginRequiredMixin,UpdateView):
+    template_name = 'blog/update_post.html'
+    model = Article
+    form_class = ArticleForm
+    success_url = reverse_lazy('blog:post-list')
+
+    def get_queryset(self):
+        profile = get_object_or_404(Profile,user=self.request.user)
+        return Article.objects.filter(is_active=True,author=profile)
+# _______________________________________________________
+
+class DeletePostView(LoginRequiredMixin,DeleteView):
+    template_name = 'blog/delete_post.html'
+    model = Article
+    context_object_name = 'post'
+    success_url = reverse_lazy('blog:post-list')
+
+    def get_queryset(self):
+        profile = get_object_or_404(Profile,user=self.request.user)
+        return Article.objects.filter(is_active=True,author=profile)      
+# _______________________________________________________
+
+class PostDetailsView(DetailView):
+    template_name = 'blog/blog_details.html'
+    model = Article
+    context_object_name = 'post'
+# _______________________________________________________
+
+class BlogView(View):
+    template_name = 'blog/blog_list.html'
+    def get(self,request,*args, **kwargs):
+        posts = Article.objects.filter(is_active=True).order_by('-published_date')
+        return render(request,self.template_name,{"posts":posts})
 # _______________________________________________________
