42e8348b37971dfbf01dedc397b1b79ad7748ecb
Mohamad Ep <mamad_ep@yahoo.com>
2025-11-02 12:46:04 +0330

    Chore: Create docker-compose for stage & prod area

--- core/apps/blog/tasks.py
+++ core/apps/blog/tasks.py
@@ -9,7 +9,7 @@ def get_send_email():
     artilce = Article.objects.filter(is_active=False)
     if artilce.exists():
         artilce.delete()
-        print(f"Inactive articles removed...")
+        print("Inactive articles removed...")
 
 
 # _______________________________________________________
